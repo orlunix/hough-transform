1edd061ac1a16019b03396109b439ddae12b7db1
lunix <hl.ren.pub@gmail.com>
2010-08-30 08:30:36 +0800

    version 000:only build a framwork, edge-detection arithmetric is implemented.Next,begin to go about designing hough-transform.

---
+++ sw/hough-transform-win32/edge_detection.h
@@ -0,0 +1,15 @@
+//**************************************************************************************************
+//	Author	:	hlren
+//	Email		:	hl.ren.pub@gmail.com
+//	Website	:	http://www.cnblogs.com/lunix
+//==================================================================================================
+//	File		:	edge_detection.h
+//	
+//**************************************************************************************************
+#ifndef _EDGE_DETECTION_H_
+#define _EDGE_DETECTION_H_
+
+
+void edge_detection(unsigned char * image, int width, int height);
+
+#endif
